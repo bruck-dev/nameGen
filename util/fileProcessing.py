9120f64767da65cf2164264aa834ec6607b6182b
bruck-dev <141885443+bruck-dev@users.noreply.github.com>
2024-02-05 18:15:14 -0600

    Remove generator from Python App
    1) The Python app has been replaced with a JSON editor. It's unnecessary to keep both the app and the website up to date, but the JSON editor of the tool has become out of dated and needed a refactor. It now checks the assets/namelists folder for any and all JSON files and lets you select and edit them individually, and is key agnostic.

--- util/fileProcessing.py
+++ util/fileProcessing.py
@@ -7,6 +7,26 @@ __version__ = "0.1.A"
 __status__ = "Development"
 
 import glob
+import os
 
 def getMusic():
     return glob.glob("assets/music/*.wav") + glob.glob("assets/music/*.mp3") + glob.glob("assets/music/*.flac")
+
+def getLowestSubdir(firstDir):
+    lowestDir = list()
+    for root,dirs,files in os.walk(firstDir):
+        if files and not dirs:
+            lowestDir.append(root)
+    return lowestDir
+            
+def getJson(path):
+    return glob.glob(path + '/*.json')
+
+def getJsonKeys(json):
+    keys = list(json.keys())
+    keys.remove('title')
+    keys.remove('description')
+    return keys
+
+def getImmediateSubdirs(dir):
+    return [f.path for f in os.scandir(dir) if f.is_dir()]
--- util/jsonProcessing.py
+++ util/jsonProcessing.py
@@ -9,114 +9,6 @@ __status__ = "Development"
 
 import shlex
 import json
-import random
-
-# Find namelist JSON file from passed namelist parameter
-def getNamelist(namelist:str):
-    match namelist:
-        # Human Lists
-        case 'Western':
-            file = open('assets/namelists/fantasy/human/western.json', encoding='utf-8')
-        case 'Eastern':
-            file = open('assets/namelists/fantasy/human/eastern.json', encoding='utf-8')
-        case 'Northern':
-            file = open('assets/namelists/fantasy/human/northern.json', encoding='utf-8')
-        
-        # Elf Lists
-        case 'High Elf':
-            file = open('assets/namelists/fantasy/elf/highElf.json', encoding='utf-8')
-        case 'Wood Elf':
-            file = open('assets/namelists/fantasy/elf/woodElf.json', encoding='utf-8')
-        case 'Dark Elf':
-            file = open('assets/namelists/fantasy/elf/darkElf.json', encoding='utf-8')
-        case 'Drow':
-            file = open('assets/namelists/fantasy/elf/drow.json', encoding='utf-8')
-        
-        # Dwarf Lists
-        case 'Dwarf':
-            file = open('assets/namelists/fantasy/dwarf/dwarf.json', encoding='utf-8')
-            
-        # Halfling Lists
-        case 'Halfling':
-            file = open('assets/namelists/fantasy/halfling/halfling.json', encoding='utf-8')
-            
-        # Tiefling Lists
-        case 'Infernal':
-            file = open('assets/namelists/fantasy/tiefling/infernal.json', encoding='utf-8')
-        case 'Virtue':
-            file = open('assets/namelists/fantasy/tiefling/virtue.json', encoding='utf-8')
-            
-        # Orc Lists
-        case 'Orc':
-            file = open('assets/namelists/fantasy/orc/orc.json', encoding='utf-8')
-        
-        # Epithet Lists
-        case 'Suffixes':
-            file = open('assets/namelists/fantasy/shared/epithets/suffixes.json', encoding='utf-8')
-        case 'Nicknames':
-            file = open('assets/namelists/fantasy/shared/epithets/nicknames.json', encoding='utf-8')
-        case 'Animals':
-            file = open('assets/namelists/fantasy/shared/epithets/animals.json', encoding='utf-8')
-        case 'Sobriquets':
-            file = open('assets/namelists/fantasy/shared/epithets/sobriquets.json', encoding='utf-8')
-            
-        # Title Lists
-        case 'Nobility':
-            file = open('assets/namelists/fantasy/shared/titles/noble.json', encoding='utf-8')
-        case 'Military':
-            file = open('assets/namelists/fantasy/shared/titles/military.json', encoding='utf-8')
-        case 'Religious':
-            file = open('assets/namelists/fantasy/shared/titles/religious.json', encoding='utf-8')
-        case 'Occupation':
-            file = open('assets/namelists/fantasy/shared/titles/occupation.json', encoding='utf-8')
-    return file
-
-# Names are assembled in order, starting with prefix titles and ending with epithets
-def generateNameFromJson(namelist:str, gender:str, surnameEnabled:bool=True, epithetType:str=None, titleType:str=None):
-    
-    name = ''
-    if titleType is not None:
-        data = json.load(getNamelist(titleType))
-        match gender:
-            case 'Male':
-                name += random.choice(list(set(data['neutral'] + data['male']))) + ' '
-            case 'Female':
-                name += random.choice(list(set(data['neutral'] + data ['female']))) + ' '
-            case 'Neutral':
-                name += random.choice(data['neutral'])+ ' '
-                
-    data = json.load(getNamelist(namelist))
-
-    match gender:
-        case 'Male':
-            name += random.choice(data['male'])
-        case 'Female':
-            name += random.choice(data['female'])
-        case 'Neutral':
-            name += random.choice(data['neutral'])
-            
-    if surnameEnabled:
-        if data['surname'] and data['surname'][0] != '': # Check if the surname list isn't empty, and isn't disabled by having a blank entry in index 1
-            surname = random.choice(data['surname'])
-            if namelist == 'Northern' and gender == 'Female': # Nordic and Slavic names used to be patronymic, so this adds the 'daughter of' variant. Keeps the leading 's' for possessive.
-                if surname.endswith('ssen') or surname.endswith('sson'):
-                    surname = surname[0:-3] + random.choice(['dottir', 'datter', 'dotter'])
-                elif surname.endswith('sen') or surname.endswith('son'):
-                    surname = surname[0:-2] + random.choice(['dottir', 'datter', 'dotter'])
-                elif surname.endswith('ov'):
-                    surname = surname[0:-3] + 'a'
-            name = name + ' ' + surname
-            
-    if epithetType is not None:
-        data = json.load(getNamelist(epithetType)) # fix this to use different namelists
-        match gender:
-            case 'Male':
-                name += ' ' + random.choice(list(set(data['neutral'] + data['male'])))
-            case 'Female':
-                name += ' ' + random.choice(list(set(data['neutral'] + data ['female'])))
-            case 'Neutral':
-                name += ' ' + random.choice(data['neutral'])               
-    return name
 
 # Split names into list along spaces, ignoring single quotes
 def splitNames(string:str):
@@ -127,7 +19,7 @@ def splitNames(string:str):
     return list(split)
 
 # Add passed names to namelist, or replace if told to do so
-def addNamesToJson(names:str, namelist:str, type:str, replaceNames:bool=False):
+def addToJson(names:str, path:str, key:str, replace:bool):
 
     # Clean up the passed block of names as best as possible
     names = names.replace('\n', ' ')
@@ -141,86 +33,30 @@ def addNamesToJson(names:str, namelist:str, type:str, replaceNames:bool=False):
         namesList.remove(' ')
         
     # Get the JSON file's data, append the names to the type, sort, and save
-    file = getNamelist(namelist)
-    data = json.load(file)
-
-    match type:
-        case 'Male':
-            if replaceNames:
-                data['male'].clear()
-            for name in namesList:
-                if '"' in name:
-                    data['male'].append(name.replace('"',''))
-                else:
-                    data['male'].append(name)
-            data['male'] = list(set(data['male']))
-            data['male'].sort()
-
-        case 'Female':
-            if replaceNames:
-                data['female'].clear()
-            for name in namesList:
-                if '"' in name:
-                    data['female'].append(name.replace('"',''))
-                else:
-                    data['female'].append(name)
-            data['female'] = list(set(data['female']))
-            data['female'].sort()
-            
-        case 'Surname':
-            if replaceNames:
-                data['surname'].clear()
-            for name in namesList:
-                if '"' in name:
-                    data['surname'].append(name.replace('"',''))
-                else:
-                    data['surname'].append(name)
-            data['surname'] = list(set(data['surname']))
-            data['surname'].sort()
-        
-        case 'Neutral':
-            if replaceNames:
-                data['neutral'].clear()
-            for name in namesList:
-                if '"' in name:
-                    data['neutral'].append(name.replace('"',''))
-                else:
-                    data['neutral'].append(name)
-            data['neutral'] = list(set(data['neutral']))
-            data['neutral'].sort()
-                
-    with open(file.name, 'w',encoding='utf-8') as f:
+    data = json.load(open(path, encoding='utf-8'))
+    
+    if replace:
+        data[key].clear()
+    for name in namesList:
+        if '"' in name:
+            data[key].append(name.replace('"',''))
+        else:
+            data[key].append(name)
+    data[key] = list(set(data[key]))
+    data[key].sort()
+    
+    with open(path, 'w',encoding='utf-8') as f:
         json.dump(data, f, indent=4,ensure_ascii=False)
             
 # Return a string containing all names in passed namelist and type
-def dumpNamesFromJson(namelist:str, type:str):
+def dumpFromJson(path, key):
     
-    data = json.load(getNamelist(namelist))
+    data = json.load(open(path, encoding='utf-8'))
     newText = ''
     
-    match type:
-        case 'Male':
-            for name in data['male']:
-                if ' ' in name:
-                    newText = newText + '"' + name + '" '
-                else:
-                    newText = newText + name + ' '
-        case 'Female':
-            for name in data['female']:
-                if ' ' in name:
-                    newText = newText + '"' + name + '" '
-                else:
-                    newText = newText + name + ' '
-        case 'Surname':
-            for name in data['surname']:
-                if ' ' in name:
-                    newText = newText + '"' + name + '" '
-                else:
-                    newText = newText + name + ' '
-        case 'Neutral':
-            for name in data['neutral']:
-                if ' ' in name:
-                    newText = newText + '"' + name + '" '
-                else:
-                    newText = newText + name + ' '
+    for name in data[key]:
+        if ' ' in name:
+            newText = newText + '"' + name + '" '
+        else:
+            newText = newText + name + ' '
     return newText[:-1] # Remove extra space from the end of the string
